4f9e590feb7c1ce907dbb0d8c0643984ea9efe63
GaelTikeng <gaelinho@yahoo.fr>
2025-02-28 17:02:42 +0100

    added theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
+import { Navbar } from "@/components/organisms/navbar";
 
 export default function Home() {
   return (
-    <div className="font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-4xl font-bold">Hello, world!</h1>
-     
+    <div className="font-[family-name:var(--font-geist-sans)] md:w-2/3 mx-auto ">
+      <Navbar />     
     </div>
   );
 }
